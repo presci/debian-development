ccdccf22559eeab63357b2bb3e6c34083d1847c5
prasad iyer <p.c.iyer@gmail.com>
2020-03-24 11:49:55 -0700

    Update init.el

--- emacs-setup/init.el
+++ emacs-setup/init.el
@@ -1,3 +1,3 @@
-(add-to-list 'load-path "~/lib/emacs/haskell-mode/")
+(add-to-list 'load-path "~/.emacs.d/lib/haskell-mode/")
 (require 'haskell-mode-autoloads)
-(add-to-list 'Info-default-directory-list "~/lib/emacs/haskell-mode/")
+(add-to-list 'Info-default-directory-list "~/.emacs.d/lib/haskell-mode/")
